2c6fef07427621026b7e8b5f71376f6575fb3241
Chris <chris@starforge.co.uk>
2011-09-15 11:56:08 +0100

    Updated session handler gc settings name to be more unique.

--- SessionHandler.pm
+++ SessionHandler.pm
@@ -444,9 +444,9 @@ sub session_cleanup {
     my $timelimit = $now - $self -> {"auth"} -> get_config("session_length");
 
     # We only want to run the garbage collect occasionally
-    if($self -> {"settings"} -> {"config"} -> {"lastgc"} < $now - $self -> {"auth"} -> get_config("session_gc")) {
+    if($self -> {"settings"} -> {"config"} -> {"Session:lastgc"} < $now - $self -> {"auth"} -> get_config("session_gc")) {
         # Okay, we're due a garbage collect, update the config to reflect that we're doing it
-        $self -> {"settings"} -> set_db_config($self -> {"dbh"}, $self -> {"settings"} -> {"database"} -> {"settings"}, "lastgc", $now);
+        $self -> {"settings"} -> set_db_config($self -> {"dbh"}, $self -> {"settings"} -> {"database"} -> {"settings"}, "Session:lastgc", $now);
 
         # Remove expired guest sessions first
         my $nukesess = $self -> {"dbh"} -> prepare("DELETE FROM ".$self -> {"settings"} -> {"database"} -> {"sessions"}.
