5eecb71a0f8b8e9c64a2f8871d432c9d1a926ee2
Chris <chris@starforge.co.uk>
2012-03-23 15:43:56 +0000

    Updating documentation layout to reduce clutter a bit.

--- makedocs.sh
+++ makedocs.sh
@@ -3,4 +3,5 @@
 # Get the latest tag annotation out of git.
 VERS=`git tag -n1 | tail -n1 | perl -e '$tag = <STDIN>; $tag =~ s/^(.*?)\s\s+(.*)$/$1 - $2/; print $tag;'`
 
-(cat Doxyfile; echo "PROJECT_NUMBER = \"$VERS\"") | doxygen -
+# Generate the documentation with the project number updated with the tag.
+(cat supportfiles/Doxyfile; echo "PROJECT_NUMBER = \"$VERS\"") | doxygen -
