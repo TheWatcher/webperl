37f09b7218e2870e8d6217e371f59879865e1fad
Chris <chris@starforge.co.uk>
2012-06-15 11:11:27 +0100

    Gah, tyops.

--- Template.pm
+++ Template.pm
@@ -593,7 +593,7 @@ sub message_box {
                                                                "***message***" => $button -> {"message"}});
         }
         # Shove into the bar
-        $buttonbar $self -> load_template("messagebox_button.tem", {"***buttons***" => $buttonlist});
+        $buttonbar = $self -> load_template("messagebox_button.tem", {"***buttons***" => $buttonlist});
     }
 
     return $self -> load_template("messagebox.tem", { "***title***"      => $title,
