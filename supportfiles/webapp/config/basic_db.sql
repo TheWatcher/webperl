06a417790a585b1fdb8adaa8b50a8f7aa9f1733f
Chris <chris@starforge.co.uk>
2012-08-09 12:29:24 +0100

    Updated schema to refelect new email handling system.

--- supportfiles/webapp/config/basic_db.sql
+++ supportfiles/webapp/config/basic_db.sql
@@ -6,20 +6,6 @@
 -- Generation Time: Apr 16, 2012 at 10:32 PM
 -- Server version: 5.1.56
 -- PHP Version: 5.3.10-pl0-gentoo
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `aatl`
---
-
 -- --------------------------------------------------------
 
 --
@@ -32,7 +18,7 @@ CREATE TABLE `auth_methods` (
   `priority` tinyint(4) NOT NULL COMMENT 'The authentication method''s priority. -128 = max, 127 = min',
   `enabled` tinyint(1) NOT NULL COMMENT 'Is this auth method usable?',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Stores the authentication methods supported by the system';
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Stores the authentication methods supported by the system';
 
 -- --------------------------------------------------------
 
@@ -60,7 +46,46 @@ CREATE TABLE `blocks` (
   `args` varchar(128) NOT NULL COMMENT 'Arguments passed verbatim to the block module',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='web-accessible page modules';
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='web-accessible page modules';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email_queue`
+--
+
+CREATE TABLE `email_queue` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created` int(10) unsigned NOT NULL COMMENT 'The unix timestamp of when this message was created',
+  `creator_id` int(10) unsigned DEFAULT NULL COMMENT 'Who created this message (NULL = system)',
+  `deleted` int(10) unsigned DEFAULT NULL COMMENT 'Timestamp of message deletion',
+  `deleted_id` int(10) unsigned DEFAULT NULL COMMENT 'Who deleted the message?',
+  `subject` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'The message subject',
+  `body` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'The message body',
+  `format` enum('text') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'text' COMMENT 'Message format, for possible extension',
+  `status` enum('pending','sent','failed') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'pending' COMMENT 'What is the status of the message?',
+  `send_after` int(10) unsigned DEFAULT NULL COMMENT 'Send message after this time (NULL = as soon as possible)',
+  `sent_time` int(10) unsigned DEFAULT NULL COMMENT 'When was the last send attempt?',
+  `error_message` text COLLATE utf8_unicode_ci COMMENT 'Error message if sending failed.',
+  PRIMARY KEY (`id`),
+  KEY `created` (`created`),
+  KEY `deleted` (`deleted`),
+  KEY `result` (`status`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Stores mails to be sent through Emailer:: modules';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email_recipients`
+--
+
+CREATE TABLE `email_recipients` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email_id` int(10) unsigned NOT NULL COMMENT 'ID of the email this is a recipient entry for',
+  `recipient_id` int(10) unsigned NOT NULL COMMENT 'ID of the user sho should get the email',
+  PRIMARY KEY (`id`),
+  KEY `email_id` (`email_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Stores the recipients of emails';
 
 -- --------------------------------------------------------
 
@@ -76,7 +101,7 @@ CREATE TABLE `log` (
   `logtype` varchar(64) NOT NULL COMMENT 'The event type',
   `logdata` varchar(255) DEFAULT NULL COMMENT 'Any data that might be appropriate to log for this event',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Stores a log of events in the system.';
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Stores a log of events in the system.';
 
 -- --------------------------------------------------------
 
@@ -90,7 +115,7 @@ CREATE TABLE `modules` (
   `perl_module` varchar(128) NOT NULL COMMENT 'Name of the perl module in blocks/ (no .pm extension!)',
   `active` tinyint(1) unsigned NOT NULL COMMENT 'Is this module enabled?',
   PRIMARY KEY (`module_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Available site modules, perl module names, and status';
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='Available site modules, perl module names, and status';
 
 -- --------------------------------------------------------
 
@@ -124,7 +149,6 @@ CREATE TABLE `session_keys` (
   PRIMARY KEY (`key_id`,`user_id`),
   KEY `last_login` (`last_login`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Autologin keys';
-
 
 -- --------------------------------------------------------
 
