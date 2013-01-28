bb0b6c1049cde403f3fcf794d4b6d2bb4b5f7f82
Chris <chris@starforge.co.uk>
2013-01-28 15:17:31 +0000

    Fixed angled brackets in GPL URL (thanks jerith)

--- Webperl/Logger.pm
+++ Webperl/Logger.pm
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
 ## @class
 # A class to handle logging operations throughout a system. This collects
