7fd919a628cf4abbaa647eeec2d548acf01050ae
Chris <chris@starforge.co.uk>
2013-10-24 23:35:45 +0100

    Too many parenthesis.

--- Webperl/ConfigMicro.pm
+++ Webperl/ConfigMicro.pm
@@ -143,12 +143,12 @@ sub read {
 			$self -> {$section} -> {$1} = $2;
 
         # Handle attributes without quoted values - # or ; at any point will mark comments
-		} elsif(!$self -> {"inline_comments"} && $line =~ /^\s*((.*?\w))\s*=\s*(.+)$/ ) {
+		} elsif(!$self -> {"inline_comments"} && $line =~ /^\s*(.*?\w)\s*=\s*(.+)$/ ) {
             my $key = $1;
 			$self -> {$section} -> {$key} = $2;
             $self -> {$section} -> {$key} =~ s/^\s*(.*?)\s*$/$1/;
 
-		} elsif($self -> {"inline_comments"} && $line =~ /^\s*((.*?\w))\s*=\s*([^;#]+)/ ) {
+		} elsif($self -> {"inline_comments"} && $line =~ /^\s*(.*?\w)\s*=\s*([^;#]+)/ ) {
             my $key = $1;
 			$self -> {$section} -> {$key} = $2;
             $self -> {$section} -> {$key} =~ s/^\s*(.*?)\s*$/$1/;
